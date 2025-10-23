27fedb7415c3025ee15bf4bc018d7c5bd3c76ddc
yaderpr <yaderpineda011@gmail.com>
2025-10-22 21:40:37 -0600

    feat(payment-service): crea la estructura inicial del servicio

---
+++ api-microservices/payment-service/src/main/java/org/springshop/payment_service/config/initializer/DotenvInitializer.java
@@ -0,0 +1,16 @@
+package org.springshop.payment_service.config.initializer;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.lang.NonNull;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
+    
+    @Override
+    public void initialize(@NonNull ConfigurableApplicationContext applicationContext) {
+        // Carga las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
