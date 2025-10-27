100cc150da9fc2c8e61c9b9664726e159d6cdd52
yaderpr <yaderpineda011@gmail.com>
2025-10-26 22:39:29 -0600

    feat: configura el punto de entrada de los microservicios - completo

--- api-microservices/shipment-service/src/main/java/org/springshop/shipment_service/ShipmentServiceApplication.java
+++ api-microservices/shipment-service/src/main/java/org/springshop/shipment_service/ShipmentServiceApplication.java
@@ -2,8 +2,10 @@ package org.springshop.shipment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAsync
 public class ShipmentServiceApplication {
